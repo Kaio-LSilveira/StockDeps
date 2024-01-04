6fe96bb53a1359a17e7ca7defd642e1cfe37855d
Kaio Leote Silveira <kaiolsilveira5@gmail.com>
2024-01-04 12:12:17 -0300

    Last Commit

---
+++ assets/app/js/procurarClientes.js
@@ -0,0 +1,33 @@
+const inputProcurar = document.getElementById('procurar-cliente');
+
+const listaDeClientes = document.getElementById('client-list');
+
+function criaCardsProcurarCliente(event) {
+    const textoPesquisado = event.target.value.toLowerCase();
+  
+    listaDeClientes.innerHTML = '';
+  
+    clientes.forEach(function(clientes) {
+      const nome = clientes.nome.toLowerCase();
+  
+      if (nome.includes(textoPesquisado)) {
+        const card = document.createElement('div');
+        card.classList.add('clientes-card');
+        card.innerHTML = `
+          <a href="" class="link-clientes">
+            <h6>${clientes.nome}</h6>
+          </a>
+        `;
+        listaDeClientes.appendChild(card);
+      }
+    });
+  
+    if (textoPesquisado == "") {
+      listaDeClientes.style.display = 'none';
+    } else {
+      listaDeClientes.style.display = "block";
+    }
+  }
+  
+  inputProcurar.addEventListener('keyup', criaCardsProcurarCliente);
+  inputProcurar.addEventListener('change', criaCardsProcurarCliente);
